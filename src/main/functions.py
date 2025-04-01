1671028c29d481d0145c53d3e27e8507245578fb
maral <erdenemaral@grinnell.edu>
2025-04-01 16:34:18 -0500

    proccessing images complete

--- src/main/functions.py
+++ src/main/functions.py
@@ -1,4 +1,7 @@
-class FunctionsClass:
+from PIL import Image
+import numpy as np
+
+class ImageClass:
 
 	# fields:
 	# array of images are given
@@ -8,12 +11,27 @@ class FunctionsClass:
 	# Gives a single image and makes it a bitmap representation
 	@staticmethod
 	def img_bitmap(img):
-		return("processed!")
+		imgp = Image.open(img)
+		ary = np.array(imgp)
+
+		# Split the three channels
+		r,g,b = np.split(ary, 3, axis=2)
+		r=r.reshape(-1)
+		g=r.reshape(-1)
+		b=r.reshape(-1)
+
+		# Standard RGB to grayscale 
+		bitmap = 0.299 * r + 0.587 * g + 0.114 * b
+		bitmap = np.array(bitmap).reshape([ary.shape[0], ary.shape[1]])
+		bitmap = np.dot((bitmap > 128).astype(float),255)
+		im = Image.fromarray(bitmap.astype(np.uint8))
+		im.save(img.split('.')[0] + ".bmp")
+		return(im)
 
 	# process all the images in the array through the local function
+	# returns the bitmap representation of the image and the information
 	def img_process(self):
 		processed_img = []
 		for x in range (0, len(self.img_arr)):
-			print(x)
 			processed_img.append(self.img_bitmap(self.img_arr[x]))
 		return(processed_img)
--- src/main/main.py
+++ src/main/main.py
@@ -1,9 +1,11 @@
-from functions import FunctionsClass
+from functions import ImageClass
 
-img_arr = ["src/image/1.jpg", "src/image/2.jpg", "src/image/3.jpg"]
+img_arr = ['src/image/1.jpg', 'src/image/2.jpg', 'src/image/3.jpg']
 
-img_processor = FunctionsClass(img_arr)
+img_processor = ImageClass(img_arr)
 
 processed_img = img_processor.img_process()
 
-print(processed_img)
+print("The processed image example: ")
+
+processed_img[0].show()
