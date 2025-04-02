aa04ae36780a3710de32babb1f9c9da8fa9c96dc
Wenfei Lin <wl688728@gmail.com>
2025-04-01 20:42:48 -0500

    Implement main logic for the `unfold` function
    - Code has not been tested yet.
    - Implementations of helper methods will be added later.

--- src/main/paper_folding.py
+++ src/main/paper_folding.py
@@ -4,7 +4,13 @@ class PaperFolding:
     # the constructor for now although it doesn't make sense.
     def __init__(self, img_stack, op_stack):
         """Initializes PaperFolding with an image and an operations stack."""
+        # This represents the layer(s) of the paper.
+        # The top of the stack is at the beginning of the list and the bottom 
+        # is at the end of the list. 
         self.img_stack = img_stack
+
+        # This stack will contain folds represented as coordinates of the fold
+        # line. (list of lists?)
         self.op_stack = op_stack
 
     def fold(self):
@@ -17,7 +23,30 @@ class PaperFolding:
 
     def unfold(self):
         """Simulates unfolding the paper.""" 
-        pass
+        # Unfold one layer. Reconstruct the state of the paper by combining 
+        # the images on the top and bottom of the image stack.
+
+        new_img_stack = []
+
+        while len(self.img_stack) != 0: 
+            # Take the very top and very bottom layers of the image stack.
+            folded_flap = self.img_stack.pop()
+            base_layer = self.img_stack.pop(0)
+
+            # Unfold the top layer of the paper, which is a flap. 
+            fold_line = self.op_stack.pop()
+            unfolded_flap = self._reflect(folded_flap, fold_line)
+
+            # Combine these two layers. (Reconstruct a state of the unfolded 
+            # paper?)
+            unfolded_layer = self._or_operation(unfolded_flap, base_layer)
+
+            # Add this partly unfolded paper to a new image stack.
+            new_img_stack.append(unfolded_layer)
+
+        # Keep unfolding the paper. 
+        self.img_stack = new_img_stack
+        self.unfold()
 
     # This might be moved to `main.py` later.
     def pick_solution(self):
@@ -25,3 +54,14 @@ class PaperFolding:
         folding and punching."""
         pass
     
+    # ---------------
+    # Helper Methods -- might move these to `functions.py`?
+    # ---------------
+
+    # Note (delete later): Used in the `fold` and `unfold` methods.
+    def _reflect(self, image, fold_line):
+        pass
+
+    # `image1` and `image2` are bitmaps.
+    def _or_operation(self, image1, image2):
+        pass
