cf34d00f93ec2a6d25b8d5a59296167714186ae5
maral <erdenemaral@grinnell.edu>
2025-04-09 13:44:32 -0500

    paper

--- src/main/main.py
+++ src/main/main.py
@@ -4,8 +4,18 @@ import numpy as np
 from PIL import Image
 
 # Fields: 
-# ...
+# The model:
+# Image stack keeps track of images that represent the layers of folds
+img_stack = []
+# Operations stack keeps track of the operations that are performed on the images as folds occur
+op_stack = []
+# An instance of the Image Processor is initiated
+img_processor = ImageProcessor()
+# Create a paper for the paper folding task.
+paper = Paper(img_stack, op_stack)
 
+# =============================================================================
+# Functions:
 def initialize():
     blank_img = 'src/image/start.jpg'
     # Process the unfolded paper(blank) to bitmap representation
@@ -38,36 +48,22 @@ def pick_solution(unfolded_paper, solutions):
     # Return the most similar solution.
     return np.argmax(matching_percentage)
 
-
-# The model:
-# Image stack keeps track of images that represent the layers of folds
-img_stack = []
-# Operations stack keeps track of the operations that are performed on the images as folds occur
-op_stack = [] 
-
 # Input: sequence of images that represent the state of the folded paper in each time-slice.
 # Diameter of punch: 27 px, Radius: 13.5 px
 # Size of paper: 320 x 320 px
 input_arr = ['src/image/in1.jpg', 'src/image/in2.jpg', 'src/image/in3.jpg']
 solutions_img = ['src/image/sol1.jpg', 'src/image/sol2.jpg', 'src/image/sol3.jpg', 'src/image/sol4.jpg', 'src/image/sol5.jpg']
 
-# =============================================================================
-# For testing:
-solutions_img.append('src/image/in3.jpg')
-# =============================================================================
-
-# An instance of the Image Processor is initiated with the proper input
-img_processor = ImageProcessor()
-
 # 1D array of processed bitmap images in Image type for each state
 input_bitmap = img_processor.img_process(input_arr)
 solutions_bitmap = img_processor.img_process(solutions_img)
 
+# =============================================================================
+# For testing:
+solutions_img.append('src/image/in3.jpg')
+# =============================================================================
 # Initialize operation sets up the model
-# initialize()
-
-# Create a paper for the paper folding task.
-paper = Paper(img_stack, op_stack)
+initialize()
 
 # Fold, punch a hole, and unfold the paper.
 paper.fold()
